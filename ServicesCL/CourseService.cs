661c9990a278d29fd860b1022090afc70f3ccee3
deepmala.wakade <deepmala.wakade@YH1008642DT>
2019-03-22 19:12:27 +0530

    Add project files.

---
+++ ServicesCL/CourseService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesCL;
+
+namespace ServicesCL
+{
+   public class CourseService : ICourseService
+    {
+        public Course GetCourseById(long courseId)
+        {
+            return new Course
+            {
+                CourseId = courseId,
+                Description = "Demo Course Description",
+                InstitutionId = 1,
+                Title = "Demo Course Title"
+
+            };
+        }
+    }
+}
---
+++ ServicesCL/InstitutionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntitiesCL;
+
+namespace ServicesCL
+{
+    public class InstitutionService : IInstitutionService
+    {
+        public Institution GetInstitutionById(long institutionId)
+        {
+            return new Institution
+            {
+                InstitutionId = institutionId,
+                Name="Demo Institution",
+                Address="Demo Address",
+                Telephone="9373054744"
+            
+            };
+        }
+    }
+}
